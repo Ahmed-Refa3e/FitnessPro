2fa0b2bd339fca030f899a5af143ea36fc50c3af
AbdoMeky <117687320+AbdoMeky@users.noreply.github.com>
2025-03-16 16:59:44 -0400

    ModifyPosts(Add-Delete)

--- API/Controllers/Posts/GymPostController.cs
+++ API/Controllers/Posts/GymPostController.cs
@@ -1,6 +1,8 @@
 ﻿using Core.DTOs.PostDTO;
 using Core.Entities.PostEntities;
+using Core.Interfaces.Factories;
 using Core.Interfaces.Repositories.PostRepositories;
+using Infrastructure.Repositories.PostRepositoy;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +10,51 @@ namespace API.Controllers.Posts
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GymPostController : PostControllerBase<AddGymPostDTO, GymPost>
+    public class GymPostController : ControllerBase
     {
-        public GymPostController(IPostRepository repository, IPostRepresentationRepository representationRepository) : base(repository, representationRepository)
+        protected readonly IPostRepositoryFactory _factoryRepository;
+        protected readonly IPostRepresentationRepository _representationRepository;
+        public GymPostController(IPostRepositoryFactory factoryRepository, IPostRepresentationRepository representationRepository)
         {
+            _factoryRepository = factoryRepository;
+            _representationRepository = representationRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddPost([FromQuery]AddGymPostDTO postDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var repository = _factoryRepository.CreateRepository("GYM");
+                var result = await repository.Add(postDto);
+                if (result.Id == 0)
+                {
+                    return BadRequest(result.Massage);
+                }
+                string url = Url.Action(nameof(GetPost), new { id = result.Id });
+                return Created(url, _representationRepository.Get(result.Id));
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetPost(int id)
+        {
+            var post = _representationRepository.Get(id);
+            if (post == null)
+            {
+                return NotFound("Post not found");
+            }
+            return Ok(post);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeletePost(int id)
+        {
+            var repository = _factoryRepository.CreateRepository("GYM");
+            var result = repository.Delete(id);
+            if (result.Id != 1)
+            {
+                return BadRequest(result.Massage);
+            }
+            return Ok("Post deleted successfully");
         }
     }
 }
--- Infrastructure/Repositories/PostRepositoy/PostRepository.cs
+++ Infrastructure/Repositories/PostRepositoy/PostRepository.cs
@@ -3,85 +3,32 @@ using Core.DTOs.PostDTO;
 using Core.Entities.PostEntities;
 using Core.Entities.ShopEntities;
 using Core.Interfaces.Repositories.PostRepositories;
+using Core.Utilities;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Infrastructure.Repositories.PostRepositoy
 {
-    public class PostRepository : IPostRepository
+    public abstract class PostRepository : IPostRepository
     {
-        private readonly FitnessContext _context;
-        private readonly string _storagePath = Path.Combine(Directory.GetCurrentDirectory(), "UploadedImagesForPosts");
+        protected readonly FitnessContext _context;
+        protected readonly string _storagePath = Path.Combine(Directory.GetCurrentDirectory(), "UploadedImagesForPosts");
         public PostRepository(FitnessContext context)
         {
             _context = context;
         }
 
-        public async Task<IntResult> Add(AddPostDTO postDto)
-        {
-            if (!Directory.Exists(_storagePath))
-            {
-                Directory.CreateDirectory(_storagePath);
-            }
-
-            Post newPost = postDto switch
-            {
-                //AddGymPostDTO gymPostDto => new GymPost(gymPostDto),
-                //AddCoachPostDTO coachPostDto => new CoachPost(coachPostDto),
-                //AddShopPostDTO shopPostDto => new ShopPost(shopPostDto),
-                _ => null
-            };
-            if (newPost == null)
-            {
-                return new IntResult { Massage = "Invalid post type" };
-            }
-            var uploadedFilePaths = new List<string>();
-            _context.Posts.Add(newPost);
-            using (var transaction = _context.Database.BeginTransaction())
-            {
-                try
-                {
-                    if (!postDto.Images.IsNullOrEmpty())
-                    {
-                        foreach (var image in postDto.Images)
-                        {
-                            var filePath = chickImagePath(image);
-                            if (!string.IsNullOrEmpty(filePath.Massage))
-                            {
-                                throw new Exception(filePath.Massage);
-                            }
-                            using (var stream = new FileStream(filePath.Id, FileMode.Create))
-                            {
-                                await image.CopyToAsync(stream);
-                                uploadedFilePaths.Add(filePath.Id);
-                            }
-                            newPost.PictureUrls.Add(new PostPictureUrl { Url = filePath.Id });
-                        }
-                    }
-                    _context.SaveChanges();
-                    transaction.Commit();
-                }
-                catch (Exception ex)
-                {
-                    foreach (var path in uploadedFilePaths)
-                    {
-                        if (File.Exists(path))
-                        {
-                            File.Delete(path);
-                        }
-                    }
-                    return new IntResult { Massage = ex.Message };
-                }
-            }
-            return new IntResult { Id = newPost.Id };
-        }
+        public abstract Task<IntResult> Add(AddPostDTO postDto);
+        
         public IntResult Delete(int id)
         {
             var post = GetPost(id);
@@ -134,57 +81,50 @@ namespace Infrastructure.Repositories.PostRepositoy
             }
             return new IntResult { Id = 1 };
         }
-
-
-        /*
-        public ShowPostDTO Get(int id)
+        protected async Task<IntResult> AddPicturesToPost(AddPostDTO post ,Post newPost)
         {
-            var post = GetPost(id);
-            if (post is null)
+            var uploadedFilePaths = new List<string>();
+            using (var transaction = _context.Database.BeginTransaction())
             {
-                return null;
-            }
+                try
+                {
+                    if (!post.Images.IsNullOrEmpty())
+                    {
+                        foreach (var image in post.Images)
+                        {
+                            var filePath = AddImageHelper.chickImagePath(image, _storagePath);
 
-            return post switch
-            {
-                CoachPost coachPost => new ShowCoachPostDTO(coachPost),
-                GymPost gymPost => new ShowGymPostDTO(gymPost),
-                ShopPost shopPost => new ShowShopPostDTO(shopPost),
-                _ => null
-            };
-        }
-        /*
-        public List<ShowPostDTO> ShowPagination(string traineId, int page, int pageSize)
-        {
-            /*
-            return posts.Select(post => post switch
-            {
-                CoachPost coachPost => new ShowCoachPostDTO(coachPost),
-                GymPost gymPost => new ShowGymPostDTO(gymPost),
-                ShopPost shopPost => new ShowShopPostDTO(shopPost),
-                _ => throw new InvalidOperationException("Unknown post type")
-            }).ToList();
-            return null;
-        }
-    */
-        private Post GetPost(int id) => _context.Posts.Find(id);
-        StringResult chickImagePath(IFormFile file)
-        {
-            if (file is null)
-            {
-                return new StringResult();
+                            if (!string.IsNullOrEmpty(filePath.Massage))
+                            {
+                                throw new Exception(filePath.Massage);
+                            }
+                            using (var stream = new FileStream(filePath.Id, FileMode.Create))
+                            {
+                                await image.CopyToAsync(stream);
+                            }
+                            uploadedFilePaths.Add(filePath.Id);
+                            newPost.PictureUrls.Add(new PostPictureUrl { Url = filePath.Id });
+                        }
+                    }
+                    _context.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    foreach (var path in uploadedFilePaths)
+                    {
+                        if (File.Exists(path))
+                        {
+                            File.Delete(path);
+                        }
+                    }
+                    return new IntResult { Massage = ex.Message };
+                }
             }
-            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" };
-            var fileExtension = Path.GetExtension(file.FileName).ToLower();
-            var contentType = file.ContentType.ToLower();
-            if (!allowedExtensions.Contains(fileExtension) || !contentType.StartsWith("image/"))
-            {
-                return new StringResult { Massage = "Invalid file type, Only images are allowed" };
-            }
-            var fileName = Path.GetRandomFileName() + Path.GetExtension(file.FileName);
-            var filePath = Path.Combine(_storagePath, fileName);
-            return new StringResult { Id = filePath };
+            return new IntResult { Id = newPost.Id };
         }
+         Post GetPost(int id) => _context.Posts.Find(id);
+        public 
     }
 
 }
