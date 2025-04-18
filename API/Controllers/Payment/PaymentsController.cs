b77a53a27a07986be26a784cffb735377a7febd0
Refa3e <ahmed.elshikh550@gmail.com>
2025-04-18 16:23:18 +0200

    Refactor PaymentsController for payment intent creation

--- API/Controllers/Payment/PaymentsController.cs
+++ API/Controllers/Payment/PaymentsController.cs
@@ -1,25 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Interfaces.Repositories.OnlineTrainingRepositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Stripe;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.Controllers.Payment;
 
-public class PaymentsController : BaseApiController
+public class PaymentsController(SignInManager<ApplicationUser> signInManager,IOnlineTrainingRepository repo) : BaseApiController
 {
     [HttpPost("create-payment-intent")]
+    [Authorize(Roles = "Trainee")]
     public async Task<IActionResult> CreatePaymentIntent([FromBody] CreatePaymentIntentRequest request)
     {
-        //TODO make the amount come from db and the traineeID from the token
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        ApplicationUser? user = await signInManager.UserManager.GetUserAsync(User);
+        if (user == null)
+            return Unauthorized("User not found");
+        var onlineTraining = await repo.GetByIdAsync(request.OnlineTrainingId);
+        if (onlineTraining == null)
+            return NotFound("Online training not found");
         var options = new PaymentIntentCreateOptions
         {
-            Amount = request.Amount, // Amount in cents (2000 = $20.00) 
+            Amount = (long?)onlineTraining.Price, // Amount in cents (2000 = $20.00) 
             PaymentMethodTypes = ["card"],
             Currency = "usd",
             Metadata = new Dictionary<string, string>
         {
-            { "traineeId", request.TraineeId },
-            { "onlineTrainingId", request.OnlineTrainingId.ToString()},
-            { "subscriptionType", "online" }
+            { "traineeId", user.Id },
+            { "onlineTrainingId", request.OnlineTrainingId.ToString()}
         }
         };
 
@@ -31,10 +40,6 @@ public class PaymentsController : BaseApiController
 }
 public class CreatePaymentIntentRequest
 {
-    [Required]
-    public long Amount { get; set; }
-    [Required]
-    public string TraineeId { get; set; } = null!;
     [Required]
     public int OnlineTrainingId { get; set; }
 }
